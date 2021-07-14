81398ddcc18155057ad2ee74feda9b802be35242
Scarecrow0w <daniil6547@ya.ru>
2021-07-14 15:25:29 +0300

    added responsive

--- dist/assets/index.795ceedc.js
+++
@@ -1,1 +0,0 @@
-new Swiper(".mySwiper",{spaceBetween:0,centeredSlides:!0,autoplay:{delay:5e3,disableOnInteraction:!1,pauseOnMouseEnter:!0},pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
