025d42ccb11059d50190d1451459efed8b9310fa
Scarecrow0w <daniil6547@ya.ru>
2021-07-22 16:04:05 +0300

    hamburger menu added

--- main.js
+++ main.js
@@ -23,18 +23,38 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".main-banner").style.marginTop =
     document.querySelector("header").offsetHeight + "px";
 
-  let navLinks = document.querySelector('#navLinks'),
+  let navLinks = document.querySelector("#navLinks"),
+    navLogo = document.querySelector("#navLogo"),
     navLinksOffset = 94.625;
 
   window.addEventListener("scroll", () => {
-    if (window.pageYOffset >= navLinksOffset) {
-      if (!navLinks.classList.contains("fixed")) {
-        navLinks.classList.add("fixed");
+    if (window.innerWidth >= 1024) {
+      if (window.pageYOffset >= navLinksOffset) {
+        if (!navLinks.classList.contains("fixed")) {
+          navLinks.classList.add("fixed");
+          navLogo.classList.add('fixed');
+          navLogo.classList.add('slidedown');
+          navLogo.classList.add('logo-min');
+        }
+      } else {
+        navLinks.classList.remove("fixed");
+        navLogo.classList.remove('fixed');
+        navLogo.classList.remove('slidedown');
+        navLogo.classList.remove('logo-min');
       }
-    } else {
-      navLinks.classList.remove("fixed");
     }
   });
 });
 
+// Hamburger Menu
+let btn = document.getElementById("menuBtn"),
+  navLinks = document.getElementById("navLinks");
+
+btn.onclick = () => {
+  btn.classList.toggle('open');
+
+  navLinks.classList.toggle('hidden');
+  navLinks.classList.toggle('flex');
+  navLinks.classList.toggle('slidedown');
+}
 
